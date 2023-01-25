539a8e712fffe91be4a52a8b579c5910eb433f0e
will <will.menzies@yahoo.com>
2023-01-25 11:37:08 +0000

    test :100:

--- chess/chessEngine.py
+++ chess/chessEngine.py
@@ -2,9 +2,9 @@
 stores all current state info as well as legal move calculations and move logs
 """
 
+
 class GameSate():
     def __init__(self):
-
         self.board = [
             # board displayed as arrays
             ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
@@ -18,4 +18,88 @@ class GameSate():
         self.whiteToMove = True
         self.moveLog = []
 
+    def makeMove(self, move):
+        self.board[move.startRow][move.startCol] = "--"
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        self.moveLog.append(move)
+        self.whiteToMove = not self.whiteToMove
 
+    def undoMove(self):
+        if len(self.moveLog) != 0:
+            move = self.moveLog.pop()
+            self.board[move.startRow][move.startCol] = move.pieceMoved
+            self.board[move.endRow][move.endCol] = move.pieceCaptured
+            self.whiteToMove = not self.whiteToMove
+
+
+
+    def getValidMovies(self):
+        return  self.getPossibleMoves()
+
+
+
+    def getPossibleMoves(self):
+        moves = []
+        for r in range(len(self.board)):
+            for c in range(len(self.board[r])):
+                turn = self.board[r][c][0]
+                if (turn == "w" and self.whiteToMove) and (turn == "b" and not self.whiteToMove):
+                    piece = self.board[r][c][1]
+                    if piece == "B":
+                        self. getBishopMoves(r, c, moves)
+                    elif piece == "K":
+                        self.getKingMoves(r, c, moves)
+                    elif piece == "N":
+                        self.getKnightMoves(r, c, moves)
+                    elif piece == "P":
+                        self.getPawnMoves(r, c, moves)
+                    elif piece == "Q":
+                        self.getQueenMoves(r, c, moves)
+                    elif piece == "R":
+                        self.getRookMoves(r, c, moves)
+
+    def getBishopMoves(self, r, c, moves):
+        pass
+
+    def getKingMoves(self, r, c, moves):
+        pass
+
+    def getKnightMoves(self, r, c, moves):
+        pass
+
+    def getPawnMoves(self, r, c, moves):
+        pass
+
+    def getQueenMoves(self, r, c, moves):
+        pass
+
+    def getRookMoves(self, r, c, moves):
+        pass
+
+
+
+
+
+
+
+
+class move():
+    ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4, "5": 3, "6": 2, "7": 1, "8": 0}
+    rowsToRanks = {v: k for k, v in ranksToRows.items()}
+
+    filesToCols = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
+    colsToFiles = {v: k for k, v in filesToCols.items()}
+
+    def __init__(self, startSquare, endSquare, board):
+        self.startRow = startSquare[0]
+        self.startCol = startSquare[1]
+        self.endRow = endSquare[0]
+        self.endCol = endSquare[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+
+    def getChessNotation(self):
+        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
+
+    def getRankFile(self, r, c):
+        return self.colsToFiles[c] + self.rowsToRanks[r]
--- chess/chessMain.py
+++ chess/chessMain.py
@@ -5,35 +5,138 @@ main driver file - handles user input and displays game objects
 import pygame as p
 from chess import chessEngine
 
-
-
+# Initialize pygame library
 p.init()
+
+# Set the width and height of the game screen
 width = height = 400
+
+# Set the maximum frames per second (FPS)
 maxFPS = 15
+
+# Set the dimension of the chess board (8x8)
 dimension = 8
+
+# Calculate the size of each square on the board
 squareSize = height // dimension
 
+# Create an empty dictionary to store images of chess pieces
 images = {}
-# calls a global dictionary of images
 
 
-
-
+# Function to load images of chess pieces into the global dictionary
 def loadImages():
-   pieces = ["bB", "bK", "bN", "bP", "bQ", "bR", "wB", "wK", "wN", "wP", "wQ", "wR"]
-   for piece in pieces:
-       images[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (squareSize, squareSize))
+    pieces = ["bB", "bK", "bN", "bP", "bQ", "bR", "wB", "wK", "wN", "wP", "wQ", "wR"]
+    for piece in pieces:
+        images[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (squareSize, squareSize))
 
 
+# Main function that runs the game
 def main():
+    # Initialize pygame library
+    p.init()
 
-   p.init()
-   screen = p.display.set_mode((width, height))
-   clock = p.time.Clock()
-   screen.fill(p.Color("white"))
-   gs = chessEngine.GameSate()
-   print(gs.board)
+    # Create a screen for the game
+    screen = p.display.set_mode((width, height))
 
+    # Create a clock for the game to keep track of time
+    clock = p.time.Clock()
 
-main()
+    # Fill the screen with white color
+    screen.fill(p.Color("white"))
 
+    # Create a new instance of the chess engine game state
+    gs = chessEngine.GameSate()
+
+    # Load images of chess pieces into the global dictionary
+    loadImages()
+
+    # Set the game to running
+    running = True
+
+    # Create a variable to store the selected square
+    squareSelected = ()
+
+    # Create a list to store player clicks
+    playerClicks = []
+
+    # Game loop
+    while running:
+        for e in p.event.get():
+            # Check if the player has closed the game window
+            if e.type == p.QUIT:
+                running = False
+            # Check if the player has clicked on the screen
+            elif e.type == p.MOUSEBUTTONDOWN:
+                # Get the location of the click
+                location = p.mouse.get_pos()
+
+                # Calculate the column and row of the square clicked
+                col = location[0] // squareSize
+                row = location[1] // squareSize
+
+                # Check if the square clicked is the same as the previous square selected
+                if squareSelected == (row, col):
+                    # If so, deselect the square
+                    squareSelected = ()
+                    playerClicks = []
+                else:
+                    # If not, select the square
+                    squareSelected = (row, col)
+                    playerClicks.append(squareSelected)
+
+                # Check if the player has made two clicks (one for starting square, one for ending square)
+                if len(playerClicks) == 2:
+                    # Make a move using the chess engine
+                    move = chessEngine.move(playerClicks[0], playerClicks[1], gs.board)
+
+                    # Print the chess notation of the move
+                    print(move.getChessNotation())
+
+                    # Update the game state with the move made
+
+                    gs.makeMove(move)
+
+                    # Deselect the square and clear the player clicks list
+                    squareSelected = ()
+                    playerClicks = []
+                # Check if the player has pressed the 'u' key (to undo a move)
+
+            elif e.type == p.KEYDOWN:
+                if e.key == p.K_u:
+                    # Call the undoMove function in the chess engine to undo the last move
+                    gs.undoMove()
+                    print("undone")
+        # Draw the game state on the screen
+        drawGS(screen, gs)
+
+        # Limit the game's fps
+        clock.tick(maxFPS)
+
+        # Update the screen with the new game state
+        p.display.flip()
+
+# Function to draw the game state on the screen
+def drawGS(screen, gs):
+   drawBoardSquares(screen) # Draw the squares on the board
+   drawPieces(screen, gs.board) # Draw the pieces on the squares
+
+# Function to draw the squares on the board
+def drawBoardSquares(screen):
+   colours = [p.Color("oldlace"), p.Color("sienna")]
+   for r in range(dimension):
+      for c in range(dimension):
+         colour = colours[((r+c)%2)]
+         p.draw.rect(screen, colour, p.Rect(c*squareSize, r*squareSize, squareSize, squareSize))
+
+# Function to draw the pieces on the squares
+def drawPieces(screen, board):
+   for r in range(dimension):
+       for c in range(dimension):
+           piece = board[r][c]
+           if piece != "--":
+               screen.blit(images[piece], p.Rect(c*squareSize, r*squareSize, squareSize, squareSize))
+
+# Check if the script is being run as the main file
+if __name__ == "__main__":
+   main()
